67036d16196d6a660854c744b6d4174ae2bd9790
ABW1729 <aniketwani1729@gmail.com>
2024-10-29 22:37:03 +0530

    added tests and dockerfile

---
+++ core/apis/assignments/principal.py
@@ -0,0 +1,33 @@
+from flask import Blueprint
+from core import db
+from core.apis import decorators
+from core.apis.responses import APIResponse
+from core.models.assignments import Assignment
+
+from .schema import AssignmentSchema, AssignmentGradeSchema
+principal_assignments_resources = Blueprint('principal_assignments_resources', __name__)
+
+@principal_assignments_resources.route('/assignments', methods=['GET'], strict_slashes=False)
+@decorators.authenticate_principal
+def get_assignments(p):
+    """Lists all submitted and graded assignments"""
+    assignments = Assignment.get_all_submitted_and_graded_assignments()
+    assignments_dump = AssignmentSchema().dump(assignments, many=True)
+    return APIResponse.respond(data=assignments_dump)
+
+
+@principal_assignments_resources.route('/assignments/grade', methods=['POST'], strict_slashes=False)
+@decorators.accept_payload
+@decorators.authenticate_principal
+def grade_or_regrade_assignments(p, incoming_payload):
+    """Grades or regrades an assignment by the principal"""
+    grade_or_regrade_assignment_payload = AssignmentGradeSchema().load(incoming_payload)
+    graded_or_regraded_assignment = Assignment.mark_grade(
+        _id=grade_or_regrade_assignment_payload.id,
+        grade=grade_or_regrade_assignment_payload.grade,
+        auth_principal=p,
+    )
+
+    db.session.commit()
+    graded_or_regraded_assignment_dump = AssignmentSchema().dump(graded_or_regraded_assignment)
+    return APIResponse.respond(data=graded_or_regraded_assignment_dump)
